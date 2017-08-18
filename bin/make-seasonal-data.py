ebbce0642f247a6d6034f1c0eca111810c3a7a69
Greg Wilson <greg@datacamp.com>
2017-08-18 09:24:52 -0400

    Filling in data files used in chapters 1 and 2.
    1. Add `.gitkeep` to `backup/` so that the directory will be there in Git.
    2. Changing example filenames from `.dat` to `.csv` or `.txt`.
    3. Adding `agarwal.txt` data file used in example and exercise.
    4. Adding `bin/make-seasonal-data.py` to regenerate data files used in exercises.
    5. Filling in data files used in exercises in `filesys/backup/*.csv`.
    
    Note: we could re-run `bin/make-seasonal-data.py` in `requirements.sh` to regenerate the data files rather than saving them in Git, but doing the latter means that we only need to rely on the shell to do setup.

---
+++ bin/make-seasonal-data.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+'''Make CSV data files in filesys/seasonal/*.csv.'''
+
+import sys
+import csv
+import random
+from datetime import datetime, timedelta
+
+FILENAME_TEMPLATE = './filesys/seasonal/{}.csv'
+FILENAME_STEMS = 'spring summer autumn winter'.split()
+HEADER = 'Date Tooth'.split()
+TEETH = 'incisor canine bicuspid molar wisdom'.split()
+MIN_RECORDS = 20
+MAX_RECORDS = 30
+DATE_FORMAT = '%Y-%m-%d'
+START_DATE = datetime.strptime('2017-01-01', DATE_FORMAT)
+ONE_DAY = timedelta(days=1)
+DATE_RANGE = 250
+
+def main():
+    random.seed(20170101)
+    for stem in FILENAME_STEMS:
+        path = FILENAME_TEMPLATE.format(stem)
+        records = [[datetime.strftime(START_DATE + random.randrange(DATE_RANGE) * ONE_DAY, DATE_FORMAT),
+                    random.choice(TEETH)]
+                   for r in range(random.randint(MIN_RECORDS, MAX_RECORDS))]
+        records.sort()
+        records.insert(0, HEADER)
+        csv.writer(open(path, 'w')).writerows(records)
+
+if __name__ == '__main__':
+    main()
