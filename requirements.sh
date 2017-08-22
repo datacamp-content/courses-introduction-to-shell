6cdf527d9b66438023a18f526fdd5e17e3f9cdd2
Greg Wilson <greg@datacamp.com>
2017-08-22 10:00:44 -0400

    Rewriting requirements.sh to do standalone provisioning.
    1. Modifying data file generator to use Unix line endings.
    2. Re-generating `.csv` files.
    3. Replacing `git clone` approach in `requirements.sh` with a here-doc.
    
    This doesn't scale to larger examples, but will be good enough for this course.  In future, we should support `.tar.gz` or other compressed file formats as data sets and allow `requirements.sh` to download and then unpack.

--- requirements.sh
+++ requirements.sh
@@ -1,17 +1,135 @@
-# Base name for course.
-COURSE=courses-intro-to-unix-shell
+# Make all directories.
+mkdir backup people seasonal
 
-# Go into home directory (should be `/home/repl`).
-cd
+# ./course.txt
+/bin/cat <<EOF > ./course.txt
+Introduction to the Unix Shell for Data Science
 
-# Clone the GitHub repo for the course.
-git clone git@github.com:datacamp/$(COURSE).git
+The Unix command line has survived and thrived for almost fifty years
+because it lets people to do complex things with just a few
+keystrokes. Sometimes called "the duct tape of programming", it helps
+users combine existing programs in new ways, automate repetitive
+tasks, and run programs on clusters and clouds that may be halfway
+around the world. This lesson will introduce its key elements and show
+you how to use them efficiently.
+EOF
 
-# Make sure it's on the master branch.
-git -C $(COURSE) checkout master
+# ./people/agarwal.txt
+/bin/cat <<EOF > ./people/agarwal.txt
+name: Agarwal, Jasmine
+position: RCT2
+start: 2017-04-01
+benefits: full
+EOF
 
-# Get everything out of the `filesys` directory here.
-mv $(COURSE)/filesys/* .
+# ./seasonal/winter.csv
+/bin/cat <<EOF > ./seasonal/winter.csv
+Date,Tooth
+2017-01-03,bicuspid
+2017-01-05,incisor
+2017-01-21,wisdom
+2017-02-05,molar
+2017-02-17,incisor
+2017-02-25,bicuspid
+2017-03-12,incisor
+2017-03-25,molar
+2017-03-26,incisor
+2017-04-04,canine
+2017-04-18,canine
+2017-04-26,canine
+2017-04-26,molar
+2017-04-26,wisdom
+2017-04-27,canine
+2017-05-08,molar
+2017-05-13,bicuspid
+2017-05-14,wisdom
+2017-06-17,canine
+2017-07-01,incisor
+2017-07-17,canine
+2017-08-10,incisor
+2017-08-11,bicuspid
+2017-08-11,wisdom
+2017-08-13,canine
+EOF
 
-# Get rid of the cloned repo.
-rm -rf $(COURSE)
+# ./seasonal/spring.csv
+/bin/cat <<EOF > ./seasonal/spring.csv
+Date,Tooth
+2017-01-25,wisdom
+2017-02-19,canine
+2017-02-24,canine
+2017-02-28,wisdom
+2017-03-04,incisor
+2017-03-12,wisdom
+2017-03-14,incisor
+2017-03-21,molar
+2017-04-29,wisdom
+2017-05-08,canine
+2017-05-20,canine
+2017-05-21,canine
+2017-05-25,canine
+2017-06-04,molar
+2017-06-13,bicuspid
+2017-06-14,canine
+2017-07-10,incisor
+2017-07-16,bicuspid
+2017-07-23,bicuspid
+2017-08-13,bicuspid
+2017-08-13,incisor
+2017-08-13,wisdom
+2017-09-07,molar
+EOF
+
+# ./seasonal/summer.csv
+/bin/cat <<EOF > ./seasonal/summer.csv
+Date,Tooth
+2017-01-11,canine
+2017-01-18,wisdom
+2017-01-21,bicuspid
+2017-02-02,molar
+2017-02-27,wisdom
+2017-02-27,wisdom
+2017-03-07,bicuspid
+2017-03-15,wisdom
+2017-03-20,canine
+2017-03-23,molar
+2017-04-02,bicuspid
+2017-04-22,wisdom
+2017-05-07,canine
+2017-05-09,canine
+2017-05-11,incisor
+2017-05-14,incisor
+2017-05-19,canine
+2017-05-23,incisor
+2017-05-24,incisor
+2017-06-18,incisor
+2017-07-25,canine
+2017-08-02,canine
+2017-08-03,bicuspid
+2017-08-04,canine
+EOF
+
+# ./seasonal/autumn.csv
+/bin/cat <<EOF > ./seasonal/autumn.csv
+Date,Tooth
+2017-01-05,canine
+2017-01-17,wisdom
+2017-01-18,canine
+2017-02-01,molar
+2017-02-22,bicuspid
+2017-03-10,canine
+2017-03-13,canine
+2017-04-30,incisor
+2017-05-02,canine
+2017-05-10,canine
+2017-05-19,bicuspid
+2017-05-25,molar
+2017-06-22,wisdom
+2017-06-25,canine
+2017-07-10,incisor
+2017-07-10,wisdom
+2017-07-20,incisor
+2017-07-21,bicuspid
+2017-08-09,canine
+2017-08-16,canine
+EOF
