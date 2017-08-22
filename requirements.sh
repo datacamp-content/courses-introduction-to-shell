643b947b17fc0ef9926eb00f3d071dbff3a25198
Greg Wilson <greg@datacamp.com>
2017-08-22 15:25:43 -0400

    Trying apt-get update before apt-get install

--- requirements.sh
+++ requirements.sh
@@ -8,6 +8,7 @@ cd /home/repl
 wget https://s3.amazonaws.com/assets.datacamp.com/production/course_5160/datasets/filesys.zip
 
 # Make sure we have the unzip command.
+apt-get update
 apt-get -y install unzip
 
 # Unpack.
