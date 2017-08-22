305aa345c9998e3ac726b09ff3305f45301716ac
Greg Wilson <greg@datacamp.com>
2017-08-22 13:13:04 -0400

    Filling in requirements.sh to download and unpack data.
    The new `requirements.sh` also creates an empty `backup` directory and changes ownership of everything recursively.

--- requirements.sh
+++ requirements.sh
@@ -2,12 +2,19 @@
 echo 'STARTING requirements.sh'
 
 # Get the zip file.
+wget https://s3.amazonaws.com/assets.datacamp.com/production/course_5160/datasets/filesys.zip
 
 # Unpack.
+unzip ./filesys.zip
 
 # Remove the zip file.
+rm -f ./filesys.zip
+
+# Make the `backup` directory (which starts empty).
+mkdir ./backup
 
 # Change ownership.
+chown -R repl:repl .
 
 # Report end.
 echo 'ENDING requirements.sh'
