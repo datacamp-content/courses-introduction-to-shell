101273f498f63255468acf9db1b19b0b9db17ee3
Greg Wilson <greg@datacamp.com>
2017-08-25 10:57:10 -0400

    Testing ability to provision and use correct shell scripts.
    1. Added `filesys/solutions` directory.
    2. Added `dates.sh` in that directory (correct solution for first shell script exercise in chapter 4).
    3. Created new zip file including those additions.
    4. Modified `requirements.sh` to unpack the new files and put them in `/usr/local/share/solutions`.
    5. Refactored `requirements.sh` to use variables for key elements, and to report those variables' values.

--- requirements.sh
+++ requirements.sh
@@ -1,33 +1,53 @@
+# Definitions.
+HOME_DIR=/home/repl
+SOLUTIONS_DIR=/usr/local/share/solutions
+USER_GROUP=repl:repl
+COURSE_ID=course_5160
+ARCHIVE=filesys.zip
+
 # Report start.
+echo ''
 echo '----------------------------------------'
 echo 'STARTING requirements.sh'
+echo 'HOME_DIR: ' $(HOME_DIR)
+echo 'SOLUTIONS_DIR: ' $(SOLUTIONS_DIR)
+echo 'USER_GROUP: ' $(USER_GROUP)
+echo 'COURSE_ID: ' $(COURSE_ID)
+echo 'ARCHIVE: ' $(ARCHIVE)
 
 # Make sure we're in the home directory.
-cd /home/repl
+cd $(HOME_DIR)
 
 # Get the zip file.
-wget https://s3.amazonaws.com/assets.datacamp.com/production/course_5160/datasets/filesys.zip
+wget https://s3.amazonaws.com/assets.datacamp.com/production/$(COURSE_ID)/datasets/$(ARCHIVE)
 
 # Make sure we have the unzip command.
 apt-get update
 apt-get -y install unzip
 
-# Unpack.
-unzip ./filesys.zip
+# Unpack to the local directory.
+unzip ./$(ARCHIVE)
 
 # Remove the zip file.
-rm -f ./filesys.zip
+rm -f ./$(ARCHIVE)
 
 # Make the `backup` and `bin` directories (which start off empty).
 mkdir ./backup
 
+# Move the solutions files to $(SOLUTIONS_DIR).
+rm -rf $(SOLUTIONS_DIR)
+mv ./solutions $(SOLUTIONS_DIR)
+
 # Change ownership.
-chown -R repl:repl .
+chown -R $(USER_GROUP) . $(SOLUTIONS_DIR)
 
 # Show what's been installed where.
-echo 'Installed files'
-ls -R $(pwd)/*
+echo 'Installed in home directory:'
+ls -R $(HOME_DIR)/*
+echo 'Installed in $(SOLUTIONS_DIR):'
+ls -R $(SOLUTIONS_DIR)/*
 
 # Report end.
 echo 'ENDING requirements.sh'
 echo '----------------------------------------'
+echo ''
