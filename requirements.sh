5c364cd7b16f97e1d6a96bc5cc958ffe419cdcad
Greg Wilson <greg@datacamp.com>
2017-10-11 19:01:13 -0400

    Forcing rebuild of disk image

--- requirements.sh
+++ requirements.sh
@@ -17,6 +17,7 @@ echo 'HOME_DIR: ' ${HOME_DIR}
 echo 'USER_GROUP: ' ${USER_GROUP}
 echo 'COURSE_ID: ' ${COURSE_ID}
 echo 'FILESYS: ' ${FILESYS}
+echo 'This message is to force a rebuild.'
 echo
 
 # Make sure we're in the home directory.
